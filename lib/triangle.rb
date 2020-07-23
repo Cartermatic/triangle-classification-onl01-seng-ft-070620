6ec625753ecab592732f16e7bb3efcd072de4b9a
Carter Parrett <cparrett94@hotmail.com>
2020-07-23 22:29:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,13 @@ class Triangle
   
   
   def kind
-    if 
+    if self.side1 < 0 || self.side2 < 0 || self.side3 < 0
+          raise TriangleError
+      elsif self.side1 == 0 && self.side2 == 0 && self.side3 == 0  
+          raise TriangleError
+      elsif self.side1 + self.side2 < self.side3 || self.side2 + self.side3 < self.side1 || self.side1 + self.side3 < self.side2
+          raise TriangleError
+      elsif 
   
   
   

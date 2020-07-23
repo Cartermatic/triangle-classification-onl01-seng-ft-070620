e01d8bee8accbd98521b298c046f0b9a9ae0edfb
Carter Parrett <cparrett94@hotmail.com>
2020-07-23 21:18:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,3 +18,18 @@ end
 
 
 
+def kind
+    if self.l1 == self.l2 && self.l1 == self.l3
+      "equilateral".to_sym
+    elsif self.l1 == self.l2 || self.l1 == self.l3 || self.l2 == self.l3 
+      "isosceles".to_sym 
+    elsif self.l1 != self.l2 && self.l2 != self.l3 
+      "scalene".to_sym 
+    elsif self.l1 < 0 || self.l2 < 0 || self.l3 < 0
+      raise TriangleError 
+    elsif self.l1 == 0 && self.l2 = 0 && self.l3 = 0
+      raise TriangleError
+    elsif self.l1 + self.l2 < self.l3 || self.l2 + self.l3 < self.l3 || self.l1 + self.l3 < self.l2 
+      raise TriangleError
+    end 
+  end 

6e94bde0f0f4402d077cda79cc1f5cbd59dff713
Carter Parrett <cparrett94@hotmail.com>
2020-07-23 21:08:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
 
-  attr_accessor :length_one, :length_two, :length_three
+  attr_accessor :side1, :length_two, :length_three
   
   def initialize(length_one, length_two, length_three)
     @length_one = length_one

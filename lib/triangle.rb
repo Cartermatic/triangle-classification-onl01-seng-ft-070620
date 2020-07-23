e87568e893ee8441998a5dc31ba489a3efb30498
Carter Parrett <cparrett94@hotmail.com>
2020-07-23 21:39:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,12 @@ class Triangle
       "isosceles".to_sym
       elsif self.side1 != self.side2 && self.side1 != self.side3
       "scalene".to_sym
+      elsif self.side1 < 0 || self.side2 < 0 || self.side3 < 0
+        raise TriangleError
+      elsif self.side1 == 0 && self.side2 =0 0 && self.side3 == 0  
+        raise TriangleError
+        elsif self.side1 + self.side2 < self.side3 || self.side2 + self.side3 < self.side1 || self.side1 + self.side3 < self.side2
+        raise TriangleError
     end
   end
       
@@ -29,5 +35,20 @@ class Triangle
 end
 
 
+def kind
+    if self.l1 == self.l2 && self.l1 == self.l3
+      "equilateral".to_sym
+    elsif self.l1 == self.l2 || self.l1 == self.l3 || self.l2 == self.l3 
+      "isosceles".to_sym 
+    elsif self.l1 != self.l2 && self.l2 != self.l3 
+      "scalene".to_sym 
+    elsif self.l1 < 0 || self.l2 < 0 || self.l3 < 0
+      raise TriangleError 
+    elsif self.l1 == 0 && self.l2 = 0 && self.l3 = 0
+      raise TriangleError
+    elsif self.l1 + self.l2 < self.l3 || self.l2 + self.l3 < self.l3 || self.l1 + self.l3 < self.l2 
+      raise TriangleError
+    end 
+  end 
 
 

00fa497391b4ded24f23c9696c74cde92496801e
Carter Parrett <cparrett94@hotmail.com>
2020-07-23 21:59:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,8 +20,7 @@ class Triangle
       elsif self.side1 == 0 && self.side2 == 0 && self.side3 == 0  
           raise TriangleError
         elsif self.side1 + self.side2 < self.side3 || self.side2 + self.side3 < self.side1 || self.side1 + self.side3 < self.side2
-            raise TriangleError
-      
+          raise TriangleError
     end
   end
       

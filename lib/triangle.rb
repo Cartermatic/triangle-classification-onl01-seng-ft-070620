f3e3dfa8edfab0e849c355c5b239130b87b736cb
Carter Parrett <cparrett94@hotmail.com>
2020-07-23 21:40:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
       "scalene".to_sym
       elsif self.side1 < 0 || self.side2 < 0 || self.side3 < 0
         raise TriangleError
-      elsif self.side1 == 0 && self.side2 =0 0 && self.side3 == 0  
+      elsif self.side1 == 0 && self.side2 =0 && self.side3 == 0  
         raise TriangleError
         elsif self.side1 + self.side2 < self.side3 || self.side2 + self.side3 < self.side1 || self.side1 + self.side3 < self.side2
         raise TriangleError
